91389f98ea9497a7352d1103d8219f17490aa985
Thomas Ruijters <tpj.ruijters@student.avans.nl>
2024-03-06 10:04:38 +0100

    yes

--- js/view.js
+++ js/view.js
@@ -1,8 +1,8 @@
-function birdViewFactory(bird) {
+export default function birdViewFactory(bird) {
     const elem = document.createElement('div');
     elem.classList.add('bord');
     const img = document.createElement('img');
-    img.src = 'img/' + bird.img;
+    img.src = 'img/pigeons' + bird.img;
 
     elem.appendChild(img);
 }
