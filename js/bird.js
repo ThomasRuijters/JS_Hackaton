aa6e370946da01e2ba74d502954b24b03eb75576
Thomas Ruijters <tpj.ruijters@student.avans.nl>
2024-03-06 09:35:07 +0100

    Bord

---
+++ js/bird.js
@@ -0,0 +1,22 @@
+export class Bord {
+    type;
+    img;
+    #health;
+    #package;
+    
+    grabPackage() {
+
+    }
+    dropPackage() {
+
+    }
+    kill() {
+
+    }
+}
+
+export class BordType {
+    pigeon = "pigeon";
+    eagle = "eagle";
+    owl = "owl";
+}
